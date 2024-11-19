6ed6cca852a9ef2464744c4f8ed8c399c35fb914
BraciaGenniesys <143236305+Genniesysbracia@users.noreply.github.com>
2024-11-19 19:06:22 +0800

    Create account.js

---
+++ main/js/account.js
@@ -0,0 +1,11 @@
+function showSection(sectionId) {
+    // Hide all sections
+    var sections = document.querySelectorAll('.content-section');
+    sections.forEach(function(section) {
+        section.classList.remove('active');
+    });
+
+    // Show the selected section
+    var activeSection = document.getElementById(sectionId);
+    activeSection.classList.add('active');
+}
